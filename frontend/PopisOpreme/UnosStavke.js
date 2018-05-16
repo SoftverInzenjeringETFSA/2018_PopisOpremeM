b45b2d31725b5c9fa82869c9d899888da6bcca43
lsolak <lsolak1@etf.unsa.ba>
2018-05-16 23:04:52 +0200

    popravke boje malo lol

--- frontend/PopisOpreme/UnosStavke.js
+++ frontend/PopisOpreme/UnosStavke.js
@@ -5,7 +5,8 @@ import {
     Text,
     View,
     TextInput,
-    Picker
+    Picker,
+	ScrollView
 } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import { Button } from 'react-native-elements';
@@ -70,29 +71,29 @@ export default class UnosStavke extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
+            <ScrollView style={styles.container}>
                 <Text style={styles.title}>New Inventory Item</Text>
                 <View style={styles.main}>
-                    <TextInput style={{fontSize: 15, width: '100%'}}
-                               placeholder = 'Name'
-                               placeholderTextColor = '#111'
+				<Text style={styles.label}>Name:</Text>
+                    <TextInput style={{backgroundColor: '#1fc496', height: 40}}
                                onChangeText={this.checkNameInput}
                                value={this.state.name}/>
                     <Text style={styles.label}>Description:</Text>
                     <TextInput
-                        multiline = {true} />
+                        multiline = {true}
+						style={{backgroundColor: '#1fc496', height: 80}}/>
                     <View style={styles.row}>
-                        <Text style = {{ width: '20%',  color: '#111', fontSize: 15, marginTop: 5}}>Quantity:</Text>
+                        <Text style = {{width: '30%',  color: '#fff', fontSize: 15, marginTop: 15}}>Quantity:</Text>
                         <TextInput
-                            style = {{ width: '80%'}}
+                            style = {{ backgroundColor: '#1fc496', width: '70%', height: 40}}
                             keyboardType = {'numeric'}
                             onChangeText={this.checkNumberInput}
                             value={this.state.quantity}/>
                     </View>
                     <View style={styles.row}>
-                        <Text style = {{ width: '20%',  color: '#111', fontSize: 15, marginTop: 5}}>Value:</Text>
+                        <Text style = {{width: '30%',  color: '#fff', fontSize: 15, marginTop: 15}}>Value:</Text>
                         <TextInput
-                            style = {{ width: '80%'}}
+                            style = {{ backgroundColor: '#1fc496', width: '70%', height: 40}}
                             keyboardType = {'numeric'}
                             onChangeText={this.checkValueInput}
                             value={this.state.value}/>
@@ -101,7 +102,7 @@ export default class UnosStavke extends Component {
                         <Text style={styles.label}>Category:</Text>
                         <Picker
                             mode="dropdown"
-                            style = {{ width: '50%', marginTop: 10}}
+                            style = {{height: 40, width: '50%', marginTop: 10, color: '#111', backgroundColor: '#1fc496'}}
                             selectedValue={this.state.category}
                             onValueChange={(itemValue, itemIndex) => this.setState({category: itemValue})}>
                             <Picker.Item label="Category 1" value="1" />
@@ -114,7 +115,7 @@ export default class UnosStavke extends Component {
                     <View style={styles.row}>
                         <Text style={styles.label}>Unit of measurement:</Text>
                         <Picker
-                            style = {{width: '50%', marginTop: 10}}
+                            style = {{height: 40, width: '50%', marginTop: 10, color: '#111', backgroundColor: '#1fc496'}}
                             mode="dropdown"
                             selectedValue={this.state.unit}
                             onValueChange={(itemValue, itemIndex) => this.setState({unit: itemValue})}>
@@ -128,14 +129,13 @@ export default class UnosStavke extends Component {
                     <View style={styles.row}>
                         <Text style={styles.label}>Date of purchase:</Text>
                         <DatePicker
-                            style = {{width: '50%', marginTop: 10}}
+                            style = {{width: '50%', marginTop: 10, backgroundColor: '#1fc496'}}
                             date={this.state.date}
                             mode="date"
                             placeholder="Select date"
                             format="DD-MM-YYYY"
                             minDate="01-01-2015"
-                            confirmBtnText="Confirm"
-                            cancelBtnText="Cancel"
+                            
                             customStyles={{
                                 dateIcon: {
                                     position: 'absolute',
@@ -144,15 +144,18 @@ export default class UnosStavke extends Component {
                                     marginLeft: 0
                                 },
                                 dateInput: {
+								
                                     marginLeft: 36
                                 }
                             }}
                             onDateChange={(date) => {this.setState({date: date})}}
                         />
                     </View>
-                    <Button buttonStyle = {{marginTop: 50, width: '100%', backgroundColor: '#02724e'}} title={'Create'}/>
+                    <Button containerViewStyle={{width: '100%', marginLeft: 0, marginBottom: 50, marginTop: 50}} 
+							buttonStyle = {{height: 50, backgroundColor: '#1fc496'}} 
+							title={'Create'}/>
                 </View>
-            </View>
+            </ScrollView>
         );
     }
 }
@@ -160,13 +163,13 @@ export default class UnosStavke extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#bdedde',
-        padding: 20
+        backgroundColor: '#111',
+        padding: 10
     },
     title: {
-        fontSize: 30,
+        fontSize: 35,
         textAlign: 'center',
-        color: '#00281b',
+        color: '#1fc496',
         fontWeight: 'bold'
     },
     label: {
@@ -174,13 +177,12 @@ const styles = StyleSheet.create({
         fontSize: 15,
         marginTop: 25,
         textAlign: 'left',
-        color: '#111',
+        color: '#fff',
         justifyContent: 'center'
     },
     main: {
         flex: 1,
         padding: 20,
-        paddingTop: 50,
         flexDirection: 'column'
     },
     row: {
