df9c78ba2b15b4ef8eee971fe0e7b27f1edec30c
berinas <bsmajovic1@etf.unsa.ba>
2018-05-16 14:08:38 +0200

    barkod skener-navigation

--- frontend/PopisOpreme/unos_inventurne_stavke.js
+++ frontend/PopisOpreme/unos_inventurne_stavke.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
   Platform,
   StyleSheet,
   Text,
   View,
   Button,
-  TextInput
+  TextInput,
+  Alert,
 } from 'react-native';
 import {StackNavigator,} from 'react-navigation';
+import { BarCodeScanner, Permissions, Constants, Expo } from 'expo';
 
-type Props = {};
-export default class unos_inventurne_stavke extends Component<Props> {
-  render() {
-    return (
+class unosStavkeHome extends React.Component{
+
+  render(){
+    const {navigate}= this.props.navigation;
+    const {state} = this.props.navigation;
+    return(
       <View style={styles.container}>
         <Text style={styles.welcome}>
             Unos inventurne stavke
         </Text>
         <View style={styles.skenirajbtn}>
-            <Button title={'SKENIRAJ'} /*onPress = { this.FunctionToOpenSecondActivity }*/ />
+            <Button title={'SKENIRAJ'} onPress = {()=>navigate('Profile')} />
         </View>
         <View style={styles.barkod}>
             <TextInput
@@ -52,10 +55,62 @@ export default class unos_inventurne_stavke extends Component<Props> {
     );
   }
 }
+
+class barcodeScannerScreen extends React.Component{
+  state = {
+    hasCameraPermission: null,
+  }
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({hasCameraPermission: status === 'granted'});
+    }
+
+  render() {
+    const { hasCameraPermission } = this.state;
+
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <BarCodeScanner
+            onBarCodeRead={this._handleBarCodeRead}
+            style={StyleSheet.absoluteFill}
+          />
+        </View>
+      );
+    }
+  }
+
+  _handleBarCodeRead = ({ type, data }) => {
+    Alert.alert(
+      'Alert',
+      `Bar code with type ${type} and data ${data} has been scanned!`,
+      [
+        {text: 'OK', onPress: ()=>navigate('Home'/*, {text: data}*/)},
+      ],
+      { cancelable: false }
+    )
+  }
+}
+
+const NavigationApp=StackNavigator({
+  Home: {screen: unosStavkeHome, title: 'Unos inventurne stavke'},
+  Profile: {screen: barcodeScannerScreen, title:'Scan'},
+});
+
+type Props = {};
+export default class unos_inventurne_stavke extends Component<Props> {
+  render() {
+    return <NavigationApp />;
+  }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 40,
+    marginTop: 10,
     alignItems: 'center',
   },
   welcome: {
@@ -73,7 +128,7 @@ const styles = StyleSheet.create({
   },
   evidentirajbtn: {
     width: 300,
-    marginTop: 100,
+    marginTop: 60,
   },
   barkod: {
     marginTop: 50,
